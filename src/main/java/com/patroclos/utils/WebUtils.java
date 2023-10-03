2d960d056c508b667f8cfff3357f59ce977e2b1d
patroclos <patroclos83@gmail.com>
2023-10-03 20:25:25 +0300

    disable retrieving Client IP

--- src/main/java/com/patroclos/utils/WebUtils.java
+++ src/main/java/com/patroclos/utils/WebUtils.java
@@ -22,13 +22,13 @@ public class WebUtils {
 	public String getClientIp() {
 
 		String remoteAddr = "";
-
-		if (request != null) {
-			remoteAddr = request.getHeader("X-FORWARDED-FOR");
-			if (remoteAddr == null || "".equals(remoteAddr)) {
-				remoteAddr = request.getRemoteAddr();
-			}
-		}
+//
+//		if (request != null) {
+//			remoteAddr = request.getHeader("X-FORWARDED-FOR");
+//			if (remoteAddr == null || "".equals(remoteAddr)) {
+//				remoteAddr = request.getRemoteAddr();
+//			}
+//		}
 
 		return remoteAddr;
 	}
@@ -74,33 +74,37 @@ public class WebUtils {
 
 	public static String getClientIpAddressIfServletRequestExist() {
 
-		if (RequestContextHolder.getRequestAttributes() == null) {
-			return "0.0.0.0";
-		}
+//		if (RequestContextHolder.getRequestAttributes() == null) {
+//			return "0.0.0.0";
+//		}
+//
+//		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+//		for (String header: IP_HEADER_CANDIDATES) {
+//			String ipList = request.getHeader(header);
+//			if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+//				String ip = ipList.split(",")[0];
+//				return ip;
+//			}
+//		}
 
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-		for (String header: IP_HEADER_CANDIDATES) {
-			String ipList = request.getHeader(header);
-			if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
-				String ip = ipList.split(",")[0];
-				return ip;
-			}
-		}
-
-		return request.getRemoteAddr();
+		//return request.getRemoteAddr();
+		
+		return "";
 	}
 
 	public static String getClientIpAddress(jakarta.servlet.http.HttpServletRequest request) {
 
-		for (String header: IP_HEADER_CANDIDATES) {
-			String ipList = request.getHeader(header);
-			if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
-				String ip = ipList.split(",")[0];
-				return ip;
-			}
-		}
-
-		return request.getRemoteAddr();
+//		for (String header: IP_HEADER_CANDIDATES) {
+//			String ipList = request.getHeader(header);
+//			if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+//				String ip = ipList.split(",")[0];
+//				return ip;
+//			}
+//		}
+//
+//		return request.getRemoteAddr();
+		
+		return "";
 	}
 	
 	public String getContextPath() {
